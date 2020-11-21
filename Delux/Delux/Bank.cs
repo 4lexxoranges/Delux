2eea8100895619e7c626e3796a44ec23dae7b8ab
4lexxoranges <or4nges.official@gmail.com>
2020-11-21 13:16:50 +0300

    Add new Files

--- Delux/Delux/Bank.cs
+++ Delux/Delux/Bank.cs
@@ -1,48 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace ShopCar
+namespace Delux
 {
-    class Program
-    {
-
-        static void Main(string[] args)
-        {
-            Bank bank = new Bank();
-            DillerShop dillerShop = new DillerShop();
-            string userInput;
-            Console.WriteLine("Hi, you play in the real life");
-            Console.WriteLine("Go to bank - so that check your balance in bank");
-            Console.WriteLine("1 - we go to Bank\n2 - we go to Diller Shop");
-            userInput = Console.ReadLine();
-            if (userInput == "1")
-            {
-                bank.GoToBank();
-            }
-            if (userInput == "2")
-            {
-                dillerShop.Delux();
-            }
-        }
-        public void Menu()
-        {
-            Bank bank = new Bank();
-            DillerShop dillerShop = new DillerShop();
-            string userInput;
-            Console.Clear();
-            Console.WriteLine("Hi, again. Where you want to go");
-            Console.WriteLine("1 - Go to Bank\n2 - Go to Diller Shop");
-            userInput = Console.ReadLine();
-            if (userInput == "1")
-            {
-                bank.GoToBank();
-            }
-            if (userInput == "2")
-            {
-                dillerShop.Delux();
-            }
-        }
-    }
-
     class Bank
     {
         DillerShop dillerShop = new DillerShop();
@@ -167,86 +128,4 @@ namespace ShopCar
         }
     }
 
-    //class DillerShop
-    //{
-        //Lamborghini lamborghini = new Lamborghini();
-        //Ferrari ferrari = new Ferrari();
-        //Koenigsegg koenigsegg = new Koenigsegg();
-        //Program program = new Program();
-        //string userInput;
-        //public void Delux()
-        //{
-        //    Console.Clear();
-        //    Console.WriteLine("Welcome. to DillerShop < Delux >!");
-        //    Console.WriteLine("We have Lamborghini Huracan - 102 000$,\nFerrari Spider - 152 000$\nKoenigsegg Agera R - 10 000 000$");
-        //    Console.WriteLine("1 - go to Menu\n2 - buy car");
-        //    userInput = Console.ReadLine();
-        //    if (userInput == "1")
-        //    {
-        //        program.Menu();
-        //    }
-        //    else if (userInput == "2")
-        //    {
-        //        Console.Clear();
-        //        Console.WriteLine("Ok, whats you change?");
-        //        Console.WriteLine("1 - Lambotghini Huracan\n2 - Ferrari Spider\n3 - Koenigsegg Agera R");
-        //        userInput = Console.ReadLine();
-        //        switch (userInput)
-        //        {
-        //            case "1":
-        //                lamborghini.BuyLamborghini();
-        //                break;
-        //            case "2":
-        //                ferrari.BuyFerrari();
-        //                break;
-        //            case "3":
-        //                koenigsegg.BuyKoenigsegg();
-        //                break;
-        //        }
-        //    }
-        //}
-    //}
-    class Lamborghini
-    {
-        string userInput;
-        public int lamborghiniHuracanCount;
-        public int lamborghiniHuracanInStockCount = 19;
-        public int lamborghiniHuracanPrice = 152000;
-        public int lamborghiniHP = 770;
-        public int lamborghiniMaxSpeed = 300;
-        public int lamborghiniWeight = 1600;
-        public void BuyLamborghini()
-        {
-            Console.Clear();
-            Console.WriteLine("Ok, you change Lamborghini");
-        }
-    }
-    class Ferrari
-    {
-        public int ferrariSpiderCount;
-        public int ferrariSpiderInStockCount = 20;
-        public int ferrariSpiderPrice = 291000;
-        public int ferrariHP = 699;
-        public int ferrariMaxSpeed = 260;
-        public int ferrariWeight = 1600;
-        public void BuyFerrari()
-        {
-            Console.Clear();
-            Console.WriteLine("Ok, you change Ferrari");
-        }
-    }
-    class Koenigsegg
-    {
-        public int koenigseggAgeraRCount;
-        public int koenigseggAgeraRInStockCount = 5;
-        public int koenigseggAgeraRPrice = 10000000;
-        public int koenigseggHP = 1600;
-        public int koenigseggMaxSpeed = 424;
-        public int koenigseggWeight = 1600;
-        public void BuyKoenigsegg()
-        {
-            Console.Clear();
-            Console.WriteLine("Ok, you change Koenigsegg");
-        }
-    }
 }
---
+++ Delux/Delux/DillerShop.cs
@@ -0,0 +1,47 @@
+﻿using ShopCar;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Delux
+{
+    class DillerShop
+    {
+        Lamborghini lamborghini = new Lamborghini();
+        Ferrari ferrari = new Ferrari();
+        Koenigsegg koenigsegg = new Koenigsegg();
+        Program program = new Program();
+        string userInput;
+        public void Delux()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome. to DillerShop < Delux >!");
+            Console.WriteLine("We have Lamborghini Huracan - 102 000$,\nFerrari Spider - 152 000$\nKoenigsegg Agera R - 10 000 000$");
+            Console.WriteLine("1 - go to Menu\n2 - buy car");
+            userInput = Console.ReadLine();
+            if (userInput == "1")
+            {
+                program.Menu();
+            }
+            else if (userInput == "2")
+            {
+                Console.Clear();
+                Console.WriteLine("Ok, whats you change?");
+                Console.WriteLine("1 - Lambotghini Huracan\n2 - Ferrari Spider\n3 - Koenigsegg Agera R");
+                userInput = Console.ReadLine();
+                switch (userInput)
+                {
+                    case "1":
+                        lamborghini.BuyLamborghini();
+                        break;
+                    case "2":
+                        ferrari.BuyFerrari();
+                        break;
+                    case "3":
+                        koenigsegg.BuyKoenigsegg();
+                        break;
+                }
+            }
+        }
+    }
+}
