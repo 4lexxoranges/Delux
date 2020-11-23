49406031e801b208947d04c7aabfb4d328a8381c
4lexxoranges <or4nges.official@gmail.com>
2020-11-23 22:59:18 +0300

    remove some code

--- Delux/Delux/Cars.cs
+++ Delux/Delux/Cars.cs
@@ -11,39 +11,21 @@ namespace Delux
         public int CarHP;
         public string CarColor;
         public int CarMaxSpeed;
-        public int carPrice = 102000;
+        public int carPrice;
         public int carHp;
         public string carColor;
         public int carMaxSpeed;
-        public void Lamborghini(int carPrice, int carHp, string carColor, int carMaxSpeed)
+        public Cars()
         {
-            CarPrice = carPrice;
-            CarHP = carHp;
-            CarColor = carColor;
-            CarMaxSpeed = carMaxSpeed;
-            carPrice = 102000;
-            carHp = 770;
-            carMaxSpeed = 250;
+            
         }
-        public void Ferrari(int carPrice2, int carHp2, string carColor2, int carMaxSpeed2)
+        public Cars()
         {
-            CarPrice = carPrice2;
-            CarHP = carHp2;
-            CarColor = carColor2;
-            CarMaxSpeed = carMaxSpeed2;
-            carPrice2 = 152000;
-            carHp2 = 699;
-            carMaxSpeed2 = 230;
+            
         }
-        public void Koenigsegg(int carPrice3, int carHp3, string carColor3, int carMaxSpeed3)
+        public Cars()
         {
-            CarPrice = carPrice3;
-            CarHP = carHp3;
-            CarColor = carColor3;
-            CarMaxSpeed = carMaxSpeed3;
-            carPrice3 = 10000000;
-            carHp3 = 1600;
-            carMaxSpeed3 = 400;
+            
         }
     }
 }
--- Delux/Delux/DillerShop.cs
+++ Delux/Delux/DillerShop.cs
@@ -7,89 +7,18 @@ namespace Delux
 {
     class DillerShop
     {
-        Cars cars = new Cars();
         Program program = new Program();
         Bank bank = new Bank();
         string userInput;
         public void Delux()
         {
-            
+
             Console.Clear();
             Console.WriteLine("Welcome. to DillerShop < Delux >!");
             Console.WriteLine("We have Lamborghini Huracan - 102 000$,\nFerrari Spider - 152 000$\nKoenigsegg Agera R - 10 000 000$");
             Console.WriteLine("1 - go to Menu\n2 - buy car");
-            userInput = Console.ReadLine();
-            if (userInput == "1")
-            {
-                program.Menu();
-            }
-            else if (userInput == "2")
-            {
-                Console.Clear();
-                Console.WriteLine("Ok, whats you change?");
-                Console.WriteLine("1 - Lambotghini Huracan\n2 - Ferrari Spider\n3 - Koenigsegg Agera R");
-                userInput = Console.ReadLine();
-                switch (userInput)
-                {
-                    case "1":
-                        Console.WriteLine("Which color you change");
-                        Console.WriteLine("1 - Black\n2 - Red\n3 - Orange");
-                        userInput = Console.ReadLine();
-                        if(userInput == "1")
-                        {
-                            Console.WriteLine("Ok, you buy Laborghini Aventador, black color, 770HP, Max Speed - 250km/h, weight - 1600kg");
-                            //bank.moneyUserInBank -= cars.Lamborghini(carPrice);
 
-                        }
-                        if (userInput == "2")
-                        {
-                            Console.WriteLine("Ok, you buy Laborghini Aventador, red color, 770HP, Max Speed - 250km/h, weight - 1600kg");
-                            bank.moneyUserInBank -= cars.carPrice;
-                            Console.WriteLine(bank.moneyUserInBank);
-                        }
-                        if (userInput == "3")
-                        {
-                            Console.WriteLine("Ok, you buy Laborghini Aventador, orange color, 770HP, Max Speed - 250km/h, weight - 1600kg");
 
-                        }
-
-                        break; 
-                    case "2":
-                        Console.WriteLine("Which color you change");
-                        if (userInput == "1")
-                        {
-                            Console.WriteLine("Ok, you buy Ferrari Spider, black color, 699HP, Max Speed - 230km/h, weight - 1600kg");
-
-                        }
-                        if (userInput == "2")
-                        {
-                            Console.WriteLine("Ok, you buy Ferrari Spider, red color, 669HP, Max Speed - 230km/h, weight - 1600kg");
-
-                        }
-                        if (userInput == "3")
-                        {
-                            Console.WriteLine("Ok, you buy Ferrari Spider, yellow color, 669HP, Max Speed - 230km/h, weight - 1600kg");
-
-                        }
-                        break;
-                    case "3":
-                        Console.WriteLine("Which color you change");
-                        if (userInput == "1")
-                        {
-                            Console.WriteLine("Ok, you buy Koenigegg Agera R, black color, 1600HP, Max Speed - 400km/h, weight - 1600kg");
-                            
-                        }
-                        if (userInput == "2")
-                        {
-                            Console.WriteLine("Ok, you buy Koenigegg Agera R, red color, 1600HP, Max Speed - 400km/h, weight - 1600kg");
-                        }
-                        if (userInput == "3")
-                        {
-                            Console.WriteLine("Ok, you buy Koenigegg Agera R, white color, 1600HP, Max Speed - 400km/h, weight - 1600kg");
-                        }
-                        break;
-                }
-            }
         }
     }
 }
